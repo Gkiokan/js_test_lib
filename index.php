c6aef5e3145fa6e4896bcd7e8cce30c89cf1728f
Gkiokan <gkiokan@hotmail.de>
2014-06-19 13:05:24 +0200

    First functions within the Library Testing some variable stuff out
    plug(selector).
    	.init() // just a console log statement
    	.show() // show the selected object
    	.hide() // hide the selected object
    	.debug('some notice text') // get the innerHTML of the selecotr
    	and add prepend the text from debug var.
    	.push // should push the obj in the .class container..
    	(for now TypeError ?? )
    
    thats all for now.
    later on more.

--- index.php
+++ index.php
@@ -24,7 +24,65 @@
         <br>
         <div class='debug'>Debug code?!</div>
         <script>
-            
+            (function () {
+                var plug = function(params){
+                    return new Library(params);
+                }
+                
+                var Library = function(params){
+                    var selector  = document.querySelectorAll(params),
+                    i = 0;
+                    this.length = selector.length;
+                    this.version = '0.1.0';
+                    this.url = '???';
+                    
+                    for (i;i<this.length;i++){
+                        this[i] = selector[i];
+                    }
+                    return this;
+                }
+                
+                plug.fn = Library.prototype = {
+                    hide: function(){
+                        var len = this.length;
+                        while(len--){                            
+                            this[len].style.display = 'none'
+                        }
+                    },
+                    show: function(){
+                        var len = this.length;
+                        while(len--){
+                            this[len].style.display = 'block'
+                        }
+                    },
+                    init: function(){ console.log('init'), console.log(this); },
+                    debug: function(d){
+                        var len = this.length;
+                        if (!d) d=""; else d=d;
+                        
+                        while (len--){
+                            var debug_target = this[len];
+                            var debug_content = debug_target.innerHTML;
+                            var new_content = debug_target.innerHTML = d+'<br>'+debug_content;
+                            console.log(new_content);
+                        }
+                    },
+                    push: function(){
+                        var len = this.length;
+                        while(len--){
+                            var obj = this[len];
+                            document.querySelectorAll('.debug').innerHTML(obj);
+                        }
+                    }
+                }
+                
+                if (!window.plug){
+                    window.plug = plug;
+                    window.p = plug;
+                    console.log('js test library loaded ... ');
+                }
+                
+            })();
             
         </script>
         
