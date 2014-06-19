bb42a727b70739c16076a4ee4614b87766f319bf
Gkiokan <gkiokan@hotmail.de>
2014-06-19 11:35:24 +0200

    prepare stuff

---
+++ index.php
@@ -0,0 +1,33 @@
+<?
+
+?>
+
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Js test Lib</title>
+        <style>
+            body { font-family: sans-serif; }
+            .t { border: 1px solid green; }
+            
+            
+        </style>
+    </head>
+    <body>
+        <h1>Javascript Test Library </h1>
+        
+        <div class='t' id='test'>Test ID Container</div>
+        <br>
+        <div class='t' id='test2'>Test ID 2 Container</div>
+        
+        
+        <br>
+        <div class='debug'>Debug code?!</div>
+        <script>
+            
+            
+        </script>
+        
+        
+    </body>
+</html>
